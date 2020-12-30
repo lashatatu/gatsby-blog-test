bfffeaadd5c073b1d38fd3df9b7ba99c24ab6cfb
lasha tatulashvili <lashatatu@gmail.com>
2020-12-30 15:39:10 +0400

    initial

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>hi lasha</h1>
+    <h1>hello from the third page</h1>
+  </Layout>
+)
+
+export default ThirdPage
